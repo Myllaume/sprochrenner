ff1cdea399870b9897c6f172192399323d35decc
JustineHqt <44361308+JustineHqt@users.noreply.github.com>
2018-11-04 18:09:01 +0100

    Final ?
    - Ajout du menu déroulant (pc et mobile)
    - Redirections vers les pages (liens)
    - Fixed accueil (responsive)

--- js/menu.js
+++ js/menu.js
@@ -2,5 +2,25 @@ var burgerBouton = document.getElementById('burger-bouton');
 var burgerOuvert = document.getElementById('burger-ouvert');
 
 burgerBouton.addEventListener('click', function(){
-    burgerOuvert.classList.toggle('header__navigation--deploiement--ouverture');
+    burgerOuvert.classList.toggle('header__navigation--deploiement---ouverture');
+});
+
+// OUVERTURE INFORMATIONS
+
+var informations = document.getElementById('informations');
+var infoOuvert = document.getElementById('info-ouvert');
+
+informations.addEventListener('click', function(){
+    infoOuvert.classList.toggle('header__navigation--deploiement---menu----informations-----info-ouvert');
+    informations.classList.toggle('header__navigation--deploiement---menu----links-----turquoise');
+});
+
+// OUVERTURE PARTICIPATION
+
+var participations = document.getElementById('participer');
+var partsOuvert = document.getElementById('participation-ouvert');
+
+participations.addEventListener('click', function(){
+    partsOuvert.classList.toggle('header__navigation--deploiement---menu----participer-----participation-ouvert');
+    participations.classList.toggle('header__navigation--deploiement---menu----links-----turquoise');
 });
